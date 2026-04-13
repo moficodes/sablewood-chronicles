a0396c8406d9ecb00372cf86f28e846a61208d00
Mofi Rahman <moficodes@gmail.com>
2026-04-12 23:51:19 -0400

    chore: initialize admin next.js app workspace

---
+++ admin/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased bg-gray-50 text-gray-900">
+        {children}
+      </body>
+    </html>
+  );
+}
